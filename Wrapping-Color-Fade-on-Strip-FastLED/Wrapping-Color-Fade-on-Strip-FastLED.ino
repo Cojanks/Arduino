0204732bc666a68c6c172798842be516d12e9b78
Corey Jenkins <cojanks@gmail.com>
2019-02-11 18:59:32 -0600

    Fix accidental commented out code

--- Wrapping-Color-Fade-on-Strip-FastLED/Wrapping-Color-Fade-on-Strip-FastLED.ino
+++ Wrapping-Color-Fade-on-Strip-FastLED/Wrapping-Color-Fade-on-Strip-FastLED.ino
@@ -7,9 +7,9 @@ FASTLED_USING_NAMESPACE
 #define COLOR_ORDER   GRB
 CRGB leds[NUM_LEDS];
 
-int delayval = 50; // Speed of Runner
+int delayval = 20; // Speed of Runner
 int fadeVal = 10; // This should be brightestValue / numPixelFace and should be a whole number. 
-//int numPixelFade = 20; The fade on either side
+int numPixelFade = 20; // The fade on either side
 int pixelHue = 130;
 int brightestValue = 200;
 
@@ -29,19 +29,15 @@ void loop() {
       int right = i + p; 
 
       if(left >= 0) {
-//        leds.setPixelColor(i-p, pixels.Color(0,200 - (fadeVal*p),200 - (fadeVal*p)));
         leds[i-p] = CHSV(pixelHue,255, brightestValue - (fadeVal*p));
       }
       else if(left < 0) {
-//        leds.setPixelColor(NUM_LEDS + left, pixels.Color(0,200 - (fadeVal*p),200 - (fadeVal*p)));
         leds[NUM_LEDS + left] = CHSV(pixelHue,255, brightestValue - (fadeVal*p));
       }
       if (right < NUM_LEDS) {
-//        leds.setPixelColor(i + p, pixels.Color(0,200 - (fadeVal*p),200 - (fadeVal*p)));]
         leds[i + p] = CHSV(pixelHue,255, brightestValue - (fadeVal*p));
       }
        else if(right >= NUM_LEDS) {
-//        leds.setPixelColor(right - NUM_LEDS, pixels.Color(0,200 - (fadeVal*p),200 - (fadeVal*p)));
         leds[right - NUM_LEDS] = CHSV(pixelHue,255, brightestValue - (fadeVal*p));
       }
     }
